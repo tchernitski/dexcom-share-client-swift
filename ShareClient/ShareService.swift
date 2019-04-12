57a4dc2e7393919cf7120fb330b1874967b74405
Vladimir Tchernitski <vtchernitski@gmail.com>
2019-04-12 20:26:40 +0300

    added max's server

--- ShareClient/ShareService.swift
+++ ShareClient/ShareService.swift
@@ -31,15 +31,14 @@ public class ShareService: ServiceAuthentication {
          https://github.com/dabear/NightscoutShareServer
          */
 
-        /*
-        let customServer = "https://REPLACEME"
+        
+        let customServer = "https://maxcgmserver.herokuapp.com"
         let customServerTitle = "Custom"
 
         credentials[2].options?.append(
                 (title: LocalizedString(customServerTitle, comment: "Custom share server option title"),
                 value: customServer))
-        */
-
+        
         if let username = username, let password = password, let url = url {
             isAuthorized = true
             client = ShareClient(username: username, password: password, shareServer: url.absoluteString)
